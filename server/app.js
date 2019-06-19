dd56ddef65ccadab484cf586f3d2f56281b13b4c
erwan <erwanp.vallee@gmail.com>
2019-06-19 10:56:02 +0200

    fix: maj ng8

--- server/app.js
+++ server/app.js
@@ -16,7 +16,8 @@ app.use(express.static(path.join(__dirname, '../public')));
 
 mongoose.connect('mongodb://angulardyma:123@ds119685.mlab.com:19685/angulardyma', {
     keepAlive: true,
-    reconnectTries: Number.MAX_VALUE
+    reconnectTries: Number.MAX_VALUE,
+	  useNewUrlParser: true
   }, function (error) {
     if (error) {
       console.log(error);
