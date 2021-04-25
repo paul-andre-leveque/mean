855ce331ccbdbf8d202452358c4059461f3996c2
paul-andre-leveque <p_leveque@digital-campus.net>
2021-04-25 16:04:31 +0200

    app

--- server/app.js
+++ server/app.js
@@ -1,7 +1,4 @@
 const express = require("express");
-const path = require("path");
-const logger = require("morgan");
-const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 // const index = require("./routes/index");
@@ -10,12 +7,9 @@ const articleRoutes = require('./routes/articleWikiRoute');
 
 const app = express();
 
-app.use(logger("dev"));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "../public")));
-
 
 
 
@@ -35,6 +29,12 @@ mongoose.connect(
   }
 );
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
 
 
 
